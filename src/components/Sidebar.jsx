eb7e95f91e4a408c1850e19cdef3afb69a8bdc68
DiiegoAG <diego@apdevs.com>
2024-09-03 18:20:28 -0600

    Sidebar component is added, its fetch categories to an api from fake store and show result on sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react"
+import useFetch from "../hooks/useFetch"
+
+const Sidebar = ({children}) => {
+    const [dropDown, setDropDown] = useState(true)
+    const { data, isPending, error } = useFetch('https://fakestoreapi.com/products/categories')
+
+    const handleButton = () => {
+        setDropDown(! dropDown)
+    }
+
+    return (
+        <>
+            <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                <div className="px-3 py-3 lg:px-5 lg:pl-3">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center justify-start rtl:justify-end">
+                            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                                <span className="sr-only">Open sidebar</span>
+
+                            </button>
+                            <a href="https://flowbite.com" className="flex ms-2 md:me-24">
+                            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
+                            <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+                <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+                    <ul className="space-y-2 font-medium">
+                        <li>
+                            <button type="button" className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example" onClick={handleButton}>
+                                <img className="w-8" src="./svg/products.svg" alt="" />
+                                <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Categories</span>
+                                <svg className="w-3 h-3">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+                                </svg>
+                            </button>
+                            <ul id="dropdown-example" className="hidden py-2 space-y-2" style={{display: dropDown ? 'block' : 'none'}}>
+                                {data && data.map((item, index) => (
+                                    <li key={index}>
+                                        <a href="#" className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{item}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                                <img className="w-8" src="./svg/log-out.svg" alt="" />
+                                <span className="ms-3">Log Out</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+
+            <div className="p-4 sm:ml-64">
+                <div className="p-4 mt-14">
+                        {children}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Sidebar
