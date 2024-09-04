e84822232a4593d8e8fd926abc756829975eb0b6
DiiegoAG <diego@apdevs.com>
2024-09-03 18:26:35 -0600

    Sidebar is used in APP main file, and ProductList is chnged to use as his child

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ProductList from './components/products/ProductsList';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
-    <ProductList />
+    <>
+      <Sidebar>
+        <ProductList />
+      </Sidebar>
+    </>
   )
 }
 
